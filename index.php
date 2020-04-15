9d346a6c4438c1237aa6ae41d4b4f770eaf444d6
saharei <saharai.co@gmail.com>
2020-04-15 18:31:42 +0900

    修正：execute消し漏れ

--- index.php
+++ index.php
@@ -316,7 +316,7 @@ function createRoomAndGetRoomId($userId) {
   $dbh = dbConnection::getConnection();
   $sql = 'insert into '. TABLE_NAME_ROOMS .' (userid, roomid) values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?) ';
   $sth = $dbh->prepare($sql);
-  $sth->execute(array($userId, PDO::PARAM_NULL, $roomId));
+  $sth->execute(array($userId, $roomId));
 
   return $roomId;
 }
@@ -326,7 +326,7 @@ function enterRoomAndGetRoomId($userId, $roomId) {
   $dbh = dbConnection::getConnection();
   $sql = 'insert into '. TABLE_NAME_ROOMS .' (userid, roomid) SELECT pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ? where exists(select roomid from ' . TABLE_NAME_ROOMS . ' where roomid = ?) returning roomid';
   $sth = $dbh->prepare($sql);
-  $sth->execute(array($userId, PDO::PARAM_NULL, $roomId, $roomId));
+  $sth->execute(array($userId, $roomId, $roomId));
   if (!($row = $sth->fetch())) {
     return PDO::PARAM_NULL;
   } else {
