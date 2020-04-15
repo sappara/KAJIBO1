506c8fc795d5a12683e7a7e98e5be182ab92ea8b
saharei <saharai.co@gmail.com>
2020-04-15 14:34:54 +0900

    追加：DB接続

--- index.php
+++ index.php
@@ -435,5 +435,37 @@ function replyTextMessage($bot, $replyToken, $text) {
       error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
     }
   }
+
+
+// データベースへの接続を管理するクラス
+class dbConnection {
+  // インスタンス
+  protected static $db;
+  // コンストラクタ
+  private function __construct() {
+
+    try {
+      // 環境変数からデータベースへの接続情報を取得し
+      $url = parse_url(getenv('DATABASE_URL'));
+      // データソース
+      $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+      // 接続を確立
+      self::$db = new PDO($dsn, $url['user'], $url['pass']);
+      // エラー時例外を投げるように設定
+      self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $e) {
+      error_log('Connection Error: ' . $e->getMessage());
+    }
+  }
+  
+  // シングルトン。存在しない場合のみインスタンス化
+  public static function getConnection() {
+    if (!self::$db) {
+      new dbConnection();
+    }
+    return self::$db;
+  }
+}
   
 ?>
