c3bbfd59ed23a051bb8c1861e8b372b612814574
saharei <saharai.co@gmail.com>
2020-04-19 15:39:53 +0900

    修正：flexmessageをjsonで 再修正

--- index.php
+++ index.php
@@ -400,7 +400,8 @@ if (!$response->isSucceeded()) {
 
   // $response = $bot->replyMessage($replyToken, $FlexMessageBuilder);
 function replyFlexMessage($bot, $replyToken, $altText, $FlexMessage) {
-  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\FlexMessageBuilder($altText, $FlexMessage));
+  $containerBuilder = new \LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder($FlexMessage);
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\FlexMessageBuilder($altText, $containerBuilder));
   if (!$response->isSucceeded()) {
     error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
