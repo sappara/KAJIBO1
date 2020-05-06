8c711897e92cf7aae5a14d9637e9625167488c85
saharei <saharai.co@gmail.com>
2020-05-06 10:18:14 +0900

    修正：カスタム登録ボタン押下時に表示するflexMessage一部修正

--- index.php
+++ index.php
@@ -142,6 +142,7 @@ foreach ($events as $event) {
       // cmd_modification_menu
       else if(substr($event->getPostbackData(), 4) == 'modification_menu'){
         $bot->replyMessage($event->getReplyToken(), new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('カスタマイズのメニューへ', linkToUser(getenv('CHANNEL_ACCESS_TOKEN'), $event->getUserId(), 'richmenu-483be03d906642db37c9bf40a14c421b')));
+        // PHP Fatal error:  Uncaught TypeError: Argument 2 passed to LINE\LINEBot::replyMessage() must be an instance of LINE\LINEBot\MessageBuilder, instance of LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder given, called in /app/index.php on line 144 and defined in /app/vendor/linecorp/line-bot-sdk/src/LINEBot.php:125
       }
       // cmd_main_menu
       else if(substr($event->getPostbackData(), 4) == 'main_menu'){
@@ -208,17 +209,19 @@ foreach ($events as $event) {
 
           $boxTextComponentsTitle = [new \LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder('５）洗剤の収納場所',null,null,'lg',null, null, true, null, null, '#0d1b2a')];
           $boxComponentsTitleBuilder = array();
-          foreach($boxTextComponentsTitle as $value){
-            array_push($boxComponentsTitleBuilder,$value);
-          }
+          array_push($boxComponentsTitleBuilder,$boxTextComponentsTitle);
+          // foreach($boxTextComponentsTitle as $value){
+          //   array_push($boxComponentsTitleBuilder,$value);
+          // }
           $layout3 = new \LINE\LINEBot\Constant\Flex\ComponentLayout;
           $boxComponentsTitle = new \LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder($layout3::VERTICAL, $boxComponentsTitleBuilder);
           
           $boxTextComponentsText = [new \LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder('洗剤は「〇〇」を探してください。',null,null,null,null, null, true, null, null, '#0d1b2a')];
           $boxComponentstextBuilder = array();
-          foreach($boxTextComponentsText as $value){
-            array_push($boxComponentsTextBuilder,$value);
-          }
+          array_push($boxComponentsTextBuilder,$boxTextComponentsText);
+          // foreach($boxTextComponentsText as $value){
+          //   array_push($boxComponentsTextBuilder,$value);
+          // }
           $layout4 = new \LINE\LINEBot\Constant\Flex\ComponentLayout;
           $boxComponentsText = new \LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder($layout4::VERTICAL, $boxComponentsTextBuilder);
 
@@ -233,7 +236,7 @@ foreach ($events as $event) {
           $layout = new \LINE\LINEBot\Constant\Flex\ComponentLayout;
           $heroImageUrl = 'https://' . $_SERVER['HTTP_HOST'] .  '/img/object_27.jpg';
           $aspectMode = new \LINE\LINEBot\Constant\Flex\ComponentImageAspectMode;
-          replyFlexMessage($bot, $event->getReplyToken(), '家事マニュアルの登録', $layout::VERTICAL, $headerTextComponents, $bodyBoxComponents, $heroImageUrl, $aspectMode::COVER
+          replyFlexMessageForModification($bot, $event->getReplyToken(), '家事マニュアルの登録', $layout::VERTICAL, $headerTextComponents, $bodyBoxComponents, $heroImageUrl, $aspectMode::COVER
           );
         }
       }
